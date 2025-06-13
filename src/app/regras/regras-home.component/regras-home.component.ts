5297e347c840ef5f1f55310113de97058823eca7
Marano <77554696+MaranoMMV@users.noreply.github.com>
2025-06-13 18:53:02 -0300

    Adicionado as regras

--- src/app/regras/regras-home.component/regras-home.component.ts
+++ src/app/regras/regras-home.component/regras-home.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import {MatTabsModule} from '@angular/material/tabs';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterModule} from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatTableModule} from '@angular/material/table';
 
 @Component({
   selector: 'app-regras-home.component',
-  imports: [],
+  imports: [MatTabsModule
+    , HttpClientModule,
+    RouterModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatTableModule,
+    MatTabsModule],
   templateUrl: './regras-home.component.html',
   styleUrl: './regras-home.component.css'
 })
